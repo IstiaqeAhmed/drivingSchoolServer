cce01da7f26ce96d42247c7f668e72e183fcf5be
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-04-20 23:23:20 +0600

    bit change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fs = require("fs-extra");
 const fileUpload = require("express-fileupload");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
@@ -28,6 +29,7 @@ client.connect((err) => {
   const admittanceCollection = client
     .db("learnDriving")
     .collection("admittance");
+  const trainerCollection = client.db("learnDriving").collection("trainers");
 
   app.post("/addAdmittance", (req, res) => {
     const admittance = req.body;
@@ -45,24 +47,59 @@ client.connect((err) => {
 
   app.post("/admittanceByDate", (req, res) => {
     const date = req.body;
-    console.log(date.date);
-    admittanceCollection.find({ date: date.date }).toArray((err, documents) => {
-      res.send(documents);
+    // console.log(date.date);
+    const email = req.body.email;
+    trainerCollection.find({ email: email }).toArray((err, trainers) => {
+      const filter = { date: date.date };
+      if (trainers.length === 0) {
+        filter.email = email;
+      }
+
+      admittanceCollection.find(filter).toArray((err, documents) => {
+        res.send(documents);
+      });
     });
   });
 
   app.post("/addATrainer", (req, res) => {
     const file = req.files.file;
-
     const name = req.body.name;
     const email = req.body.email;
-    console.log(name, email, file);
-    file.mv(`${__dirname}/trainers/${file.name}`, (err) => {
+    // console.log(name, email, file);
+    const filePath = `${__dirname}/trainers/${file.name}`;
+
+    file.mv(filePath, (err) => {
       if (err) {
         console.log(err);
         return res.status(500).send({ msg: "Failed to upload Image" });
       }
-      return res.send({ name: file.name, path: `/${file.name}` });
+      const newImg = fs.readFileSync(filePath);
+      const encImg = newImg.toString("base64");
+
+      var image = {
+        contentType: req.files.file.mimetype,
+        size: req.files.file.size,
+        img: Buffer.from(encImg, "base64"),
+      };
+
+      // return res.send({ name: file.name, path: `/${file.name}` });
+      trainerCollection.insertOne({ name, email, image }).then((result) => {
+        fs.remove(filePath, (error) => {
+          if (error) {
+            console.log(error);
+            res.status(500).send({ msg: "Failed to upload Image" });
+          }
+          res.send(result.insertedCount > 0);
+        });
+        // res.send(result.insertedCount > 0);
+      });
+    });
+  });
+
+  app.post("/isTrainer", (req, res) => {
+    const email = req.body.email;
+    trainerCollection.find({ email: email }).toArray((err, trainers) => {
+      res.send(trainers.length > 0);
     });
   });
 });
